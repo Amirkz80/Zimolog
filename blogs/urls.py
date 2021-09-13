c17459ca17041478a5be05aa503e8d267a5e350b
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-09-14 00:57:09 +0430

    Adding dashboard page

--- blogs/urls.py
+++ blogs/urls.py
@@ -9,8 +9,10 @@ urlpatterns= [
     path('add_post/', views.add_post, name='add_post' ),
     # page that edits posts
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
-    # a page to show the user this post does't belong to him/her
+    # page to show the user this post does't belong to him/her
     path('prompt/', views.edit_post, name='prompt'),
-    # a page that shows serach results
+    # page that shows serach results
     path('search/', views.search, name='search'),
+    # page that shows user's profile and posts
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
