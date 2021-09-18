2621ab71eda2e8443534018e6da80002d00c2070
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-09-18 18:58:36 +0430

    adding comments model to the blogs/models.py

--- blogs/models.py
+++ blogs/models.py
@@ -13,4 +13,18 @@ class BlogPost(models.Model):
         if len(self.text) >= 30:
             return (f"{self.title}, {self.text[:31]}...") 
         else:
-            return (f"{self.title}, {self.text}")    
+            return (f"{self.title}, {self.text}")
+
+class Comments(models.Model):
+    """Contains text and the date of the user's comment"""
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        """Retrurns a represntation of the model"""
+        if len(self.text) >= 30:
+            return (f"{self.text[:31]}...") 
+        else:
+            return (f"{self.text}")
