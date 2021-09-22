a389c88d3ff1c4d9cb09f52c14b19ccdbb1bdcae
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-09-22 16:42:24 +0330

    adding comment's form

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,9 +1,18 @@
 from django import forms
-from .models import BlogPost
+from .models import BlogPost,Comments
 
 class BlogPostForm(forms.ModelForm):
+    """A class to make forms for posts"""
     class Meta:
         model = BlogPost
         fields = ['title', 'text']
         labels = {'title': 'Post Title', 'text': 'Text' }
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
+
+class CommentsForm(forms.ModelForm):
+    """A class to make forms for new comments"""
+    class Meta:
+        model = Comments
+        fields = ['text']
+        labels = {'text' : ''}
+        widgets = {'text': forms.Textarea(attrs={'cols' : 80})}
