e2dd6024065ca07831782dbc32e466426fc80a62
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-10-02 18:19:33 +0330

    Changing UserInfo model relationship from ManyToOne to OneToOne

--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default='')
     followers_number = models.PositiveIntegerField(default=0)
     following_number = models.PositiveIntegerField(default=0)
     followers = models.JSONField(default=list) 
     following = models.JSONField(default=list)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} info"
