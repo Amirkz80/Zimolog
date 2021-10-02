69929ecf491b587b73febccd38c5bc58a6339dad
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-10-02 15:00:27 +0330

    defining and adding userinfo model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserInfo(models.Model):
+    followers_number = models.PositiveIntegerField(default=0)
+    following_number = models.PositiveIntegerField(default=0)
+    followers = models.JSONField(default=list) 
+    following = models.JSONField(default=list)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
