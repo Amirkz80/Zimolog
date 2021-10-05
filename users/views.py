1cf77b80fce930f6bfcc1ea3d4d120a50b7f1da3
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-10-05 20:13:41 +0330

    editing register views in users and adding user_info func to users.views

--- users/views.py
+++ users/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import REDIRECT_FIELD_NAME, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from users.models import UserInfo
 from blogs.models import BlogPost
 
 
+def get_user_info(req, user_name_string):
+    """"Gets UserInfo that is conected to the the user"""
+    user = User.objects.get(username=user_name_string)
+    return (user)
+
 def register(request):
     """Registers new user"""
+    
+    # Check if user is authenticated
+    if request.user.is_authenticated :
+        return redirect('users:dashboard')
+
     if request.method != 'POST':
         # Sets form to a blank page
         form = UserCreationForm()
@@ -17,6 +28,8 @@ def register(request):
 
             if form.is_valid():
                 new_user = form.save()
+                # creating a userinfo model for user
+                UserInfo(user=new_user).save()
                 # now we log in and then redirect to the index page
                 login(request, new_user)
                 return redirect('blogs:index')
@@ -38,22 +51,34 @@ def dashboard(request):
     user_posts = BlogPost.objects.filter(owner=request.user).order_by("-date_added")
 
     context = {'user_posts' : user_posts, 'date_joined' : date_joined}
-    return render(request, 'registration/dashboard.html', context)
+    return render(request, 'user/dashboard.html', context)
 
 
 @login_required
 def followers(request,user_name):
     """Shows user's followers"""
-    user = User.objects.get(username=user_name)
-
-    context = {'user' : user}
-    return render(request, 'registration/followers.html', context)
+    user = get_user_info(request, user_name)
+    context ={'user' : user}
+    return render(request, 'user/followers.html', context)
 
 
 @login_required
 def following(request,user_name):
     """Shows user's following"""
-    user = User.objects.get(username=user_name)
-
+    user = get_user_info(request, user_name)
     context = {'user' : user}
-    return render(request, 'registration/following.html', context)    
+    return render(request, 'user/following.html', context)
+
+
+@login_required
+def user_info(request, user_name): 
+    """Returns user's(not authenticated user) posts and info"""
+
+    # If currnet authenticated user is trying to see h/h page, redirect to h/h dashboard
+    if request.user.username == user_name:
+        return redirect('users:dashboard')
+    
+    user = get_user_info(request, user_name)
+    posts = user.blogpost_set.order_by("-date_added")
+    context = {'user' : user, 'posts' : posts}
+    return render(request, 'user/user_info.html', context)       
