4d1897128c8db28c5262ce00b0e0cdeb9e098521
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-10-10 00:05:01 +0330

    adding calculate_time fucn

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from time import strftime
 from django.shortcuts import render, redirect
 from django.contrib.auth import REDIRECT_FIELD_NAME, login
 from django.contrib.auth.forms import UserCreationForm
@@ -6,6 +8,63 @@ from django.contrib.auth.models import User
 from users.models import UserInfo
 from blogs.models import BlogPost
 
+
+def calculate_time(object_time, now_time, keyword=''):
+    """Calculates how much time has been passed from object_time till now"""
+    """keyword can be (posted) word or (joined) word depending on usage for posts or users"""
+    
+    final_string = ""
+
+    diff = {
+    'year' : now_time.year - object_time.year,
+    'month' : now_time.month - object_time.month,
+    'day' :  now_time.day - object_time.day,
+    'hour' :  now_time.hour - object_time.hour,
+    'minute' :  now_time.minute - object_time.minute,
+    'second' : now_time.second - object_time.second
+    }
+    
+    if diff['year'] > 0 :
+        final_string = object_time.strftime(f"{keyword} at %b, %d %Y")
+
+    if diff['year'] == 0 and diff['month'] != 0 :
+        final_string = object_time.strftime(f"{keyword} at %b, %d")
+
+    if diff['year'] == 0 and diff ['month'] == 0 :
+
+        # For days
+        if diff['day'] > 7 :
+            final_string = object_time.strftime(f"{keyword} at %b, %d")
+        if diff['day'] <= 7 and diff['day'] > 1 :
+            final_string = f"{keyword} {diff['day']} days ago"
+        if diff['day'] == 1 :
+            final_string = f"{keyword} {diff['day']} day ago"
+        
+        # For hours
+        if diff['day'] == 0 :
+            if diff['hour'] > 1 :
+                final_string = f"{keyword} {diff['hour']} hours ago"
+            if diff['hour'] == 1 :
+                final_string = f"{keyword} {diff['hour']} hour ago"
+
+            # For minutes
+            if diff['hour'] == 0 :
+                if diff['minute'] > 1 :
+                    final_string = f"{keyword} {diff['minute']} minutes ago"
+                if diff['minute'] == 1 :
+                    final_string = f"{keyword} {diff['minute']} minute ago"
+
+                # For seconds
+                if diff['minute'] == 0 :
+                    if diff['second'] > 1 :
+                        final_string = f"{keyword} {diff['second']} seconds ago"
+                    if diff['second'] == 1 :
+                        final_string = f"{keyword} {diff['second']} second ago"
+
+                    if diff['second'] == 0 :
+                        final_string = f"{keyword} just now"
+
+    return final_string
 
 def get_user(user_name_string):
     """"Gets User from data base through its username"""
@@ -48,9 +107,14 @@ def logged_out(request):
 def dashboard(request):
     """Returns authentiacted user's informations and posts"""
     date_joined = request.user.date_joined
+    now = datetime.utcnow()
     user_posts = BlogPost.objects.filter(owner=request.user).order_by("-date_added")
+    join_message = calculate_time(date_joined, now, keyword="Joined")
+    
+    for post in user_posts:
+        post.date_added = calculate_time(post.date_added, datetime.utcnow(), keyword="posted") 
 
-    context = {'user_posts' : user_posts, 'date_joined' : date_joined}
+    context = {'user_posts' : user_posts, 'join_message' : join_message}
     return render(request, 'user/dashboard.html', context)
 
 
@@ -80,7 +144,9 @@ def user_info(request, user_name):
     
     user = get_user(user_name)
     posts = user.blogpost_set.order_by("-date_added")
-    context = {'requested_user' : user, 'posts' : posts}
+    join_message = calculate_time(user.date_joined, datetime.utcnow(), keyword="posted")
+
+    context = {'requested_user' : user, 'posts' : posts, 'join_message' : join_message}
     return render(request, 'user/user_info.html', context)
 
 
