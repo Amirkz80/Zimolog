34a165caa9ebd1f486522b5d2a203fdb7c05ab6f
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-11-01 22:35:57 +0330

    changing form field widgets

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,8 +1,14 @@
+from typing import AbstractSet
 from django import forms
 from .models import BlogPost,Comments
 
 class BlogPostForm(forms.ModelForm):
     """A class to make forms for posts"""
+    title = forms.CharField(
+        required= False,
+        help_text= 'You can add up to 100 characters to the title .',
+        widget = forms.TextInput(attrs={'placeholder' : 'This part is optional to fill'})
+    )
     class Meta:
         model = BlogPost
         fields = ['title', 'text']
