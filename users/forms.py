adce3c0b86b9e9c5185415c9ef173d42e85d49e7
Amir kazemzadeh <amirkazemzadeh77@gmail.com>
2021-11-01 22:30:15 +0330

    make a form to get profile pic and bio

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,26 @@
+from typing import Text
 from django import forms
-from django.forms.widgets import FileInput
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
+from django.forms import widgets
+from django.forms.widgets import FileInput, TextInput, Widget
 from .models import UserInfo
 
 class UserInfoForm(forms.ModelForm):
     """A form to take profile pictures"""
-    picture = forms.ImageField(required=False, widget=forms.FileInput)
+    picture = forms.ImageField(
+        label='New Picture :',
+        required=False,
+        widget=forms.FileInput,
+        help_text='- Others can see your profile pic on your posts, comments and profile '
+        )
+    bio_text = forms.CharField(
+        label= 'Bio Text :',
+        help_text='You can add up to 150 characters to yor bio .',
+        error_messages={'max_length' : 'This bio is more than 150 characters !'},
+        required=False,
+        widget = forms.TextInput(attrs={'placeholder' : 'Describe Yourslef !'})
+    )        
     class Meta :
         model = UserInfo
         fields = ['picture', 'bio_text']
