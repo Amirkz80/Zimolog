28ca311a1c13a9f69f9d48863cdc4a08602072a0
Amir <amirkazemzadeh77@gmail.com>
2022-01-10 21:22:40 +0330

    add email form urk

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,26 +5,40 @@ app_name = 'blogs'
 urlpatterns= [
     # Home page
     path('', views.index, name='index'),
+    
     # About page
     path('about/', views.about ,name='about' ),
+    
     # Page that adds new post
     path('add_post/', views.add_post, name='add_post' ),
-    # page that edits posts
+    
+    # Page that edits posts
     path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
-    # page to show the user this post does't belong to him/her
+    
+    # Page to show the user this post does't belong to him/her
     path('prompt/', views.edit_post, name='prompt'),
-    # page that shows serach results
+    
+    # Page that shows serach results
     path('search/', views.search, name='search'),
-    # deletes the post
+    
+    # Deletes the post
     path('delete/<int:post_id>/', views.delete, name='delete'),
-    # page that shows full post with its comments
+    
+    # Page that shows full post with its comments
     path('post/<int:post_id>', views.full_post, name='full_post'),
-    # adds a comment
+    
+    # Adds a comment
     path('add_comment/<int:post_id>', views.add_comment, name='add_comment'),
-    # like or dislike a post
+    
+    # Like or dislike a post
     path('like/<int:post_id>', views.like, name='like'),
-    # discover page
+    
+    # Discover page
     path('discover/', views.discover, name='discover'),
-    # discover page by hearts
-    path('discover/<str:sort_type>', views.discover, name='discover')
+    
+    # Discover page by hearts
+    path('discover/<str:sort_type>', views.discover, name='discover'),
+    
+    # Share via email form page
+    path('post/<int:post_id>/share', views.share, name='share')
 ]
